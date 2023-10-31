9d3a317224fe36e8bfa7faf337dbaa5c984ab4a3
PunkBull <eugene.t@frfi.cc>
2023-10-31 17:32:52 +0300

    main functions changed

--- README.md
+++ README.md
@@ -1,70 +1,31 @@
-# Getting Started with Create React App
+## Usage
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. **Upload Addresses**: Click the "Choose File" button and select a JSON file containing an array of Ethereum wallet addresses.
 
-## Available Scripts
+2. **Add Address Manually**: Enter an Ethereum wallet address in the input field and click the "Add Address" button.
 
-In the project directory, you can run:
+3. **Generate Merkle Tree**: Once you have added all the addresses, click the "Generate Merkle Tree" button.
 
-### `npm start`
+4. **Export Merkle Tree Data**: Click the "Export Merkle Tree Data" button to download a JSON file containing the Merkle tree root and leaf hashes.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+5. **Verify Address**: Enter an Ethereum wallet address in the "Enter address to verify" input field and click the "Verify Address" button. The result will be displayed below.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Built With
 
-### `npm test`
+- [React](https://reactjs.org/) - A JavaScript library for building user interfaces
+- [merkletreejs](https://github.com/miguelmota/merkletreejs) - A JavaScript library for generating Merkle trees
+- [ethers.js](https://docs.ethers.io/v5/) - A library for interacting with the Ethereum blockchain
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Authors
 
-### `npm run build`
+* **Your Name** - *Initial work* - [PunkBull](https://github.com/thePunkBull)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## License
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Acknowledgments
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import MerkleTree from 'merkletreejs';
+import { Buffer } from 'buffer';
 import './App.css';
+import { keccak256 } from "@ethersproject/keccak256";
+import { toUtf8Bytes } from "@ethersproject/strings";
+
+window.Buffer = Buffer;
 
 function App() {
+  const [walletAddresses, setWalletAddresses] = useState([]);
+  const [root, setRoot] = useState('');
+  const [tree, setTree] = useState(null);
+  const [addressToCheck, setAddressToCheck] = useState('');
+  const [verificationResult, setVerificationResult] = useState(null);
+  const [manualAddress, setManualAddress] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [leafHashes, setLeafHashes] = useState([]);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          const addresses = JSON.parse(e.target.result);
+          if (Array.isArray(addresses)) {
+            setWalletAddresses(addresses);
+            setRoot('');
+            setTree(null);
+            setLeafHashes([]);
+          } else {
+            alert('Invalid JSON file. The file must contain an array of addresses.');
+          }
+        } catch (error) {
+          alert('Error reading file: ' + error.message);
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  const addManualAddress = () => {
+    if (manualAddress) {
+      if (walletAddresses.includes(manualAddress)) {
+        setErrorMessage('This address has already been added.');
+      } else {
+        setWalletAddresses((prev) => [...prev, manualAddress]);
+        setManualAddress('');
+        setErrorMessage('');
+      }
+    }
+  };
+
+  const generateMerkleTree = () => {
+    const leaves = walletAddresses.map((addr) =>
+      keccak256(toUtf8Bytes(addr))
+    );
+    const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+    const root = tree.getRoot().toString('hex');
+    setRoot(root);
+    setTree(tree);
+    setLeafHashes(leaves);
+  };
+  
+  const verifyAddress = () => {
+    if (tree && addressToCheck) {
+      const leaf = keccak256(toUtf8Bytes(addressToCheck));
+      const proof = tree.getHexProof(leaf);
+      const isValid = tree.verify(proof, leaf, tree.getRoot());
+      setVerificationResult(isValid);
+    } else {
+      setVerificationResult(null);
+    }
+  };
+  
+
+  const resetAll = () => {
+    setWalletAddresses([]);
+    setRoot('');
+    setTree(null);
+    setAddressToCheck('');
+    setVerificationResult(null);
+    setManualAddress('');
+    setErrorMessage('');
+    setLeafHashes([]);
+  };
+
+  const exportData = () => {
+    const data = {
+      root,
+      leafHashes,
+    };
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "merkle_tree_data.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <input type="file" onChange={handleFileUpload} />
+        <input
+          className="input"
+          type="text"
+          value={manualAddress}
+          onChange={(e) => setManualAddress(e.target.value)}
+          placeholder="Enter wallet address"
+        />
+        {walletAddresses.length > 0 && (
+          <ul>
+            {walletAddresses.map((address, index) => (
+              <li key={index}>{address}</li>
+            ))}
+          </ul>
+        )}
+        <button className="button" onClick={addManualAddress}>
+          Add Address
+        </button>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        {walletAddresses.length > 0 && (
+          <button className="button" onClick={generateMerkleTree}>
+            Generate Merkle Tree
+          </button>
+        )}
+        {leafHashes.length > 0 && (
+          <button className="button" onClick={exportData}>
+            Export Merkle Tree Data
+          </button>
+        )}
+      </div>
+      {root && <div className="root-display">Root: {root}</div>}
+      <div>
+        <input
+          className="input"
+          type="text"
+          value={addressToCheck}
+          onChange={(e) => setAddressToCheck(e.target.value)}
+          placeholder="Enter address to verify"
+        />
+        <button className="button" onClick={verifyAddress}>
+          Verify Address
+        </button>
+      </div>
+      <div className="verification-result">
+        {verificationResult !== null && (
+          <span>{verificationResult ? 'Address is valid!' : 'Address is invalid!'}</span>
+          )}
+        </div>
+        <button className="button reset-button" onClick={resetAll}>Reset All</button>
     </div>
   );
 }
